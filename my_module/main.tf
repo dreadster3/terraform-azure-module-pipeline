4210282f8df3d7fb60777a067741cbf0491c3b6c
Nuno Antunes <nuno.antunes@saint-gobain.com>
2024-01-31 16:29:49 +0000

    Enable public network access

--- my_module/main.tf
+++ my_module/main.tf
@@ -3,9 +3,10 @@ provider "azurerm" {
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = var.account_name
-  resource_group_name      = var.resource_group_name
-  location                 = var.location
-  account_tier             = var.account_tier
-  account_replication_type = var.account_replication_type
+  name                          = var.account_name
+  resource_group_name           = var.resource_group_name
+  location                      = var.location
+  account_tier                  = var.account_tier
+  account_replication_type      = var.account_replication_type
+  public_network_access_enabled = true
 }
